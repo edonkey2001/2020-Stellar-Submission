c4767f9b66c142f1bcbd98ad53a60ddc808646a3
Emmanuel Porfirio <emmanuel@omnigecko.com>
2020-10-28 18:35:11 -0400

    backend creation

---
+++ bck/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+ 
+const app = express();
+ 
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+ 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Behold The MEVN Stack!'
+    });
+});
+ 
+const port = process.env.PORT || 4000;
+const host = '0.0.0.0';
+app.listen(port, host);
